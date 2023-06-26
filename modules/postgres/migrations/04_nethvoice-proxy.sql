9803ec98531aacfcc7d6f64952d63b6830e3bdb2
Matteo Valentini <matteo.valentini@nethesis.it>
2023-06-26 14:52:46 +0200

    modules: add routes table to postgres module
    The table `nethvoice_proxy_routes` will store the configuration of the
    routes registered on the proxy.

---
+++ modules/postgres/migrations/04_nethvoice-proxy.sql
@@ -0,0 +1,28 @@
+CREATE TABLE public.nethvoice_proxy_routes (
+    id integer NOT NULL,
+    domain character varying(64) NOT NULL,
+    setid integer NOT NULL
+);
+
+ALTER TABLE ONLY public.nethvoice_proxy_routes
+    ADD CONSTRAINT nethvoice_proxy_routes_pkey PRIMARY KEY (id);
+
+CREATE SEQUENCE public.nethvoice_proxy_routes_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE public.nethvoice_proxy_routes_setid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE ONLY public.nethvoice_proxy_routes ALTER COLUMN id SET DEFAULT nextval('public.nethvoice_proxy_routes_id_seq'::regclass);
+ALTER TABLE ONLY public.nethvoice_proxy_routes ALTER COLUMN setid SET DEFAULT nextval('public.nethvoice_proxy_routes_setid_seq'::regclass);
