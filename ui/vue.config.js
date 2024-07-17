0e5e7f3e1ca732b6a02299c1ead71f62b3b63a48
Andrea Leardini <andre8244@gmail.com>
2024-07-17 17:18:47 +0200

    fix: app logo

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -8,4 +8,15 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => {
+        // Do not base64 encode images URLs. Needed to always generate module logo image
+        options.limit = -1;
+        return options;
+      });
+  },
 };
